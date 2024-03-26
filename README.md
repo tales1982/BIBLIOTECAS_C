92b6cf8bf4bb94590d5d121f5eef0ff03b6747c3
Tales Lima <102876696+tales1982@users.noreply.github.com>
2024-03-26 14:18:48 +0100

    delete arquivos

--- README.md
+++
@@ -1,2 +0,0 @@
-# BIBLIOTECAS_C
-
--- run_libft.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-rm test_printf.out
-make -C ft_printf
-cc -Wall -Werror -Wextra -o test_printf.out test_printf.c -L./ft_printf/libft -lft -L./ft_printf -lftprintf
-make fclean -C ft_printf
-./test_printf.out
