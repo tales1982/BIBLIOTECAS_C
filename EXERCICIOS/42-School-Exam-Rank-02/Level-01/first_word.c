8632d271259a5f51b0da1b16b9c1aee914df8155
Tales Lima <102876696+tales1982@users.noreply.github.com>
2024-03-26 14:38:32 +0100

    school 42

---
+++ EXERCICIOS/42-School-Exam-Rank-02/Level-01/first_word.c
@@ -0,0 +1,28 @@
+/*
+Assignment name  : first_word
+Expected files   : first_word.c
+Allowed functions: write
+--------------------------------------------------------------------------------
+Escreva um programa que receba uma string e exiba sua primeira palavra, seguida por uma
+nova linha.
+
+Uma palavra é uma seção de string delimitada por espaços/tabulações ou pelo início/fim de
+a corda.
+
+Se o número de parâmetros não for 1, ou se não houver palavras, basta exibir
+uma nova linha.
+
+Examples:
+
+$> ./first_word "FOR PONY" | cat -e
+FOR$
+$> ./first_word "this        ...    is sparta, then again, maybe    not" | cat -e
+this$
+$> ./first_word "   " | cat -e
+$
+$> ./first_word "a" "b" | cat -e
+$
+$> ./first_word "  lorem,ipsum  " | cat -e
+lorem,ipsum$
+$>
+*/
