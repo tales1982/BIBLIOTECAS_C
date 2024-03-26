47dab20e1ef05a4519e23e47f53b743ad4625f31
tales1982 <102876696+tales1982@users.noreply.github.com>
2024-03-26 21:14:13 +0100

    do_op

--- EXERCICIOS/42-School-Exam-Rank-02/Level-01/do_op/do_op.c
+++ EXERCICIOS/42-School-Exam-Rank-02/Level-01/do_op/do_op.c
@@ -62,7 +62,7 @@ int	main( int argc, char *argv[ ] )
 	if (argc != 4)
 	{ 
 		write(1, "\n", 1); // Inclui quebra de linha para clareza
-		return 1; // Encerra o programa após exibir a mensagem de erro
+		return 1; 
 	}
 	int	imputA;
 	int	imputB;
